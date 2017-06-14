0404f12b5fb39dbe09606d74836cfef2be58f18d
Squibs <z.r.holman+github@gmail.com>
2017-06-14 01:20:17 -0600

    Mostly done. More changes to come as I think of them.

--- js/main.js
+++ js/main.js
@@ -14,77 +14,6 @@ const clearItems = function () {
   }
 };
 
-const getString = function () {
-  // retrieve value of input
-  const search = document.getElementById('search').value;
-
-  // remove invalid characters and leading/trailing whitespace
-  let cleanString = search.replace(/([^A-Za-z0-9\s]+)/gi, '').trim();
-
-  // replace remaining long amount of whitespace with nothing (max 1 whitespace)
-  cleanString = cleanString.replace(/ +(?= )/g, '');
-
-  // display edited string in search box
-  document.getElementById('search').value = cleanString;
-
-  // convert string to all lowercase and replace whitespace with '+' for api call
-  cleanString = cleanString.replace(/\s/gi, '+').toLowerCase();
-
-  return cleanString;
-};
-
-const apiCall = function (string) {
-  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${string}&srlimit=9`;
-
-  console.log(string);
-  console.log(url);
-
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = () => {
-      if (req.status === 200) {
-        resolve(req.response);
-      } else {
-        reject(Error(req.statusText));
-      }
-    };
-    req.onerror = e => reject(Error(`Network Error: ${e}`));
-    req.send();
-  });
-};
-
-const generateElements = function (data) {
-  const control = data.query.search.length;
-
-  for (let i = 0; i < control; i += 1) {
-    const item = document.createElement('div');
-    const selector = document.createElement('div');
-    const content = document.createElement('div');
-    const heading = document.createElement('h1');
-    const paragraph = document.createElement('p');
-
-    item.className = 'item';
-    selector.className = 'selector';
-    content.className = 'content';
-
-    let add = document.createTextNode(data.query.search[i].title);
-    heading.innerHTML = add.textContent;
-
-    add = document.createTextNode(data.query.search[i].snippet);
-    paragraph.innerHTML = add.textContent;
-
-    content.appendChild(heading);
-    content.appendChild(paragraph);
-    item.appendChild(selector);
-    item.appendChild(content);
-
-    const body = document.getElementById('body');
-    const bodyBottom = document.getElementById('bottom');
-    body.insertBefore(item, bodyBottom);
-  }
-};
-
 const changeLayout = function () {
   const main = document.getElementById('mainContainer');
   main.style.gridRow = 1;
@@ -114,8 +43,126 @@ const changeLayout = function () {
   }
 };
 
+const getString = function () {
+  // retrieve value of input
+  const search = document.getElementById('search').value;
+
+  // remove invalid characters and leading/trailing whitespace
+  let cleanString = search.replace(/([^A-Za-z0-9\s]+)/gi, '').trim();
+
+  // replace remaining long amount of whitespace with nothing (max 1 whitespace)
+  cleanString = cleanString.replace(/ +(?= )/g, '');
+
+  // display edited string in search box
+  document.getElementById('search').value = cleanString;
+
+  // convert string to all lowercase and replace whitespace with '+' for api call
+  cleanString = cleanString.replace(/\s/gi, '+').toLowerCase();
+
+  return cleanString;
+};
+
+const apiCall = function (string) {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${string}&srlimit=9&origin=*`;
+
+  console.log(string);
+  console.log(url);
+
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = () => {
+      if (req.status === 200) {
+        resolve(req.response);
+      } else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.onerror = e => reject(Error(`Network Error: ${e}`));
+    req.send();
+  });
+};
+
+const generateElements = function (apiData) {
+  const data = JSON.parse(apiData);
+  const control = data.query.search.length;
+
+  for (let i = 0; i < control; i += 1) {
+    // elements for each item
+    const item = document.createElement('div');
+    const selector = document.createElement('div');
+    const content = document.createElement('div');
+    const heading = document.createElement('h1');
+    const paragraph = document.createElement('p');
+    const anchor = document.createElement('a');
+
+    // class names for a few elements
+    item.className = 'item';
+    selector.className = 'selector';
+    content.className = 'content';
+
+    // create link for item
+    let link = data.query.search[i].title;
+    link = link.replace(/\s/gi, '_');
+    anchor.href = `https://en.wikipedia.org/wiki/${link}`;
+    anchor.target = '_blank';
+
+    // add title to heading element
+    let add = document.createTextNode(data.query.search[i].title);
+    heading.innerHTML = add.textContent;
+
+    // add snippet to paragraph element
+    add = document.createTextNode(data.query.search[i].snippet);
+    paragraph.innerHTML = add.textContent;
+
+    // add children to correct parents
+    content.appendChild(heading);
+    content.appendChild(paragraph);
+    anchor.appendChild(selector);
+    anchor.appendChild(content);
+    item.appendChild(anchor);
+
+    // insert div item into html
+    const body = document.getElementById('body');
+    const bodyBottom = document.getElementById('bottom');
+    body.insertBefore(item, bodyBottom);
+  }
+
+  console.log('length: ' + data.query.search.length);
+  if (data.query.search.length === 0) {
+    console.log('fix it dummy');
+    const item = document.createElement('div');
+    const content = document.createElement('div');
+    const heading = document.createElement('h1');
+    const paragraph = document.createElement('p');
+
+    item.className = 'item';
+    content.className = 'content';
+
+    heading.innerHTML = 'No results';
+    paragraph.innerHTML = 'Please try a new search!';
+
+    content.appendChild(heading);
+    content.appendChild(paragraph);
+    item.appendChild(content);
+
+    const body = document.getElementById('body');
+    const bodyBottom = document.getElementById('bottom');
+    body.insertBefore(item, bodyBottom);
+  }
+
+  changeLayout();
+};
+
+let test = '';
+
 document.getElementById('submit').onclick = function () {
-  if (document.getElementById('search').value !== '') {
+  const query = document.getElementById('search').value;
+
+  // test if search query changed or is not blank
+  if (document.getElementById('search').value !== '' && query !== test) {
+    test = query;
+
     // clear any previous items
     clearItems();
 
@@ -123,12 +170,8 @@ document.getElementById('submit').onclick = function () {
     const cleanString = getString();
 
     // call api and store results
-    apiCall(cleanString).then((data) => { console.log(data.query.search[0].title); }).catch((err) => { alert(err); });
-
-    // generate elements
-    // generateElements(data);
-
-    // change layout to create room for search results
-    changeLayout();
+    apiCall(cleanString).then((data) => {
+      generateElements(data);
+    }).catch((err) => { alert(err); });
   }
 };
