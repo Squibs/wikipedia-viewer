d16d9984d1bfab76c2a89184c6661f17e60ebc6e
Squibs <z.r.holman+github@gmail.com>
2017-06-13 23:52:20 -0600

    Testing ajax.

--- js/main.js
+++ js/main.js
@@ -34,96 +34,24 @@ const getString = function () {
 };
 
 const apiCall = function (string) {
-  /*eslint-disable*/
-  const fakeAPI = {
-        "batchcomplete": "",
-        "continue": {
-            "sroffset": 9,
-            "continue": "-||"
-        },
-        "query": {
-            "searchinfo": {
-                "totalhits": 19017
-            },
-            "search": [
-                {
-                    "ns": 0,
-                    "title": "Bill Gates",
-                    "size": 90543,
-                    "wordcount": 8870,
-                    "snippet": "In 1975, <span class=\"searchmatch\">Gates</span> and Paul Allen co-founded Microsoft, which became the world's largest PC software company. During his career at Microsoft, <span class=\"searchmatch\">Gates</span> held the",
-                    "timestamp": "2017-06-12T04:31:01Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Bill & Melinda Gates Foundation",
-                    "size": 97928,
-                    "wordcount": 9215,
-                    "snippet": "<span class=\"searchmatch\">Bill</span> &amp; Melinda <span class=\"searchmatch\">Gates</span> Foundation (or the <span class=\"searchmatch\">Gates</span> Foundation, abbreviated as BMGF) is a private foundation founded by <span class=\"searchmatch\">Bill</span> and Melinda <span class=\"searchmatch\">Gates</span>. It was launched",
-                    "timestamp": "2017-06-01T02:08:06Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Bill Gates's house",
-                    "size": 5453,
-                    "wordcount": 382,
-                    "snippet": "Xanadu 2.0 is a mansion owned by <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> that overlooks Lake Washington in Medina, Washington. The 66,000-square-foot (6,100 m2) mansion is noted for",
-                    "timestamp": "2017-06-05T03:32:01Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Bill Gates (frontiersman)",
-                    "size": 1533,
-                    "wordcount": 186,
-                    "snippet": "&quot;Swiftwater&quot; <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> (died 1935) was an American frontiersman and fortune hunter, and a fixture in stories of the Klondike Gold Rush. He made and lost",
-                    "timestamp": "2016-10-29T10:50:43Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "William H. Gates Sr.",
-                    "size": 8766,
-                    "wordcount": 856,
-                    "snippet": "William Henry &quot;<span class=\"searchmatch\">Bill</span>&quot; <span class=\"searchmatch\">Gates</span> (born William Henry <span class=\"searchmatch\">Gates</span> II; November 30, 1925), also known publicly as <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> Sr. is a retired American attorney and",
-                    "timestamp": "2017-05-31T19:07:39Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Bill Gates' flower fly",
-                    "size": 2230,
-                    "wordcount": 153,
-                    "snippet": "<span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span>' flower fly (Eristalis gatesi) is a flower fly found only in Costa Rican high montane cloud forests and named after <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span>. Another fly",
-                    "timestamp": "2017-04-15T08:11:01Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Melinda Gates",
-                    "size": 17585,
-                    "wordcount": 1546,
-                    "snippet": "Microsoft Bob, Encarta, and Expedia. She met <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> while working at Microsoft. In 1994, she married <span class=\"searchmatch\">Gates</span> in a private ceremony held in Lanai, Hawaii",
-                    "timestamp": "2017-05-31T15:36:06Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Bill Gates (disambiguation)",
-                    "size": 1153,
-                    "wordcount": 157,
-                    "snippet": "<span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> (born 1955) is co-founder of Microsoft Corporation. <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span> or William <span class=\"searchmatch\">Gates</span> may also refer to: William H. <span class=\"searchmatch\">Gates</span>, Sr. (born 1925), philanthropist",
-                    "timestamp": "2017-03-26T08:55:22Z"
-                },
-                {
-                    "ns": 0,
-                    "title": "Barbarians Led by Bill Gates",
-                    "size": 2393,
-                    "wordcount": 173,
-                    "snippet": "Barbarians Led by <span class=\"searchmatch\">Bill</span> <span class=\"searchmatch\">Gates</span>: Microsoft from the Inside is a book that was jointly written by Jennifer Edstrom and Marlin Eller, an American programmer",
-                    "timestamp": "2016-08-08T11:54:18Z"
-                }
-            ]
-        }
-    };
-    /*eslint-enable*/
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${string}&srlimit=9`;
 
-  return fakeAPI;
+  console.log(string);
+  console.log(url);
+
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = () => {
+      if (req.status === 200) {
+        resolve(req.response);
+      } else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.onerror = e => reject(Error(`Network Error: ${e}`));
+    req.send();
+  });
 };
 
 const generateElements = function (data) {
@@ -187,18 +115,20 @@ const changeLayout = function () {
 };
 
 document.getElementById('submit').onclick = function () {
-  // clear any previous items
-  clearItems();
+  if (document.getElementById('search').value !== '') {
+    // clear any previous items
+    clearItems();
 
-  // get a clean string to call api with
-  const cleanString = getString();
+    // get a clean string to call api with
+    const cleanString = getString();
 
-  // call api and store results
-  const fakeAPI = apiCall(cleanString);
+    // call api and store results
+    apiCall(cleanString).then((data) => { console.log(data.query.search[0].title); }).catch((err) => { alert(err); });
 
-  // generate elements
-  generateElements(fakeAPI);
+    // generate elements
+    // generateElements(data);
 
-  // change layout to create room for search results
-  changeLayout();
+    // change layout to create room for search results
+    changeLayout();
+  }
 };
